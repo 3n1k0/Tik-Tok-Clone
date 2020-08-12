c3295129b1a4b5123e7247d9fcfac1370420f6ab
Eniko Kosztolanyi <32710850+3n1k0@users.noreply.github.com>
2020-08-12 18:44:14 +0200

    Initial commit

---
+++ src/Video.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Video.css'
+
+function Video() {
+    return (
+        <div className="video">
+            <h1>This is a video</h1>
+            {/* <VideoFooter />
+            <VideoSidebar/> */}
+        </div>
+    )
+}
+
+export default Video
